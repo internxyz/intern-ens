cedcb207ce9f3f3d2d50956d2c890b0e9295a29a
ZxStim <thang.victor.luong@gmail.com>
2025-02-25 11:02:44 +0700

    fix: changed home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Globe, AppWindowMac, MessageSquare, Rocket } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           src="/bs-logo.svg"
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <p>Get started by checking out the demos</p>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left">
           <li className="mb-2">
             <Link href="/connect-wallet">Connect wallet</Link>
           </li>
